f937adf268fd54bf60590a312ee8d4bdae16059f
angel <angel@Angel>
2025-11-17 21:13:43 -0400

    elimine la prueba

--- src/Prueba/Probando.java
+++
@@ -1,9 +0,0 @@
-package Prueba;
-
-public class Probando {
-
-		public static void main(String[] args) {
-			int num = 5;
-		System.out.println("Apunka Angel le gustan de " + num + " años" );
-		}
-}
