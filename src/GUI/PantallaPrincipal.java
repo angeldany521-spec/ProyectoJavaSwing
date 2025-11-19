14b4281de541832a91f7cebc65bf114404e26596
angel <angel@Angel>
2025-11-18 21:19:26 -0400

    size gestion pago

--- src/GUI/PantallaPrincipal.java
+++ src/GUI/PantallaPrincipal.java
@@ -36,7 +36,7 @@ public class PantallaPrincipal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 734, 511);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
